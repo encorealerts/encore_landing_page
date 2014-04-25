3222dadca994c52cc5f6f9b24291092fe723b4c4
unknown <made.in.stefano@gmail.com>
2014-04-25 12:24:32 -0300

    dev

--- public/scripts/encore.js
+++ public/scripts/encore.js
@@ -4,9 +4,5 @@ $(function (){
 		$(this).addClass('active');
 		$('#examples-content-trail').removeClass();
 		$('#examples-content-trail').addClass('scroll-' + $(this).attr('data-image'));
-		/*var top = $('#examples-content-trail').find('[data-example="' + $(this).attr('data-image') + '"]').position().top
-		setTimeout(function (){
-			$('#examples-content-trail').animate({marginTop: -top});
-		}, 10);*/
 	}).first().trigger('click');
 });
