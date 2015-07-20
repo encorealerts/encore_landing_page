f177e11e141594cfa41da5d9df28d8e4337c1454
Stéfano Zanata <made.in.stefano@gmail.com>
2015-07-20 13:49:24 -0300

    updated: all libs to cutting edge; added: workaround to act as a reverse proxy to our blog; added: better static file managment

--- app.js
+++ app.js
@@ -1,77 +1,66 @@
-﻿
-var 
-  express = require('express'),
-  app = express(),
-  assetManager = require('connect-assetmanager'),
-  oneYear = 31556908800,
-  production = !!process.env.PORT,
-  mime = require('mime');
-
-var assetManagerGroups = {
-  'css': {
-    'route': /\/encore\.min\.css/,
-    'path': __dirname + '/public/styles/',
-    'dataType': 'css',
-    'files': [ 
-      'fonts/proxima-nova/stylesheet.css', 
-      'fonts/roboto-slab/stylesheet.css',
-      'encore.css'
-    ]
-  },
-  'js': {
-    'route': /\/encore\.min\.js/,
-    'path': __dirname + '/public/scripts/',
-    'dataType': 'javascript',
-    'files': [ 
-      //'hubspot.js', 
-      'jquery-2.1.0.min.js', 
-      'funnel-animation.js',
-      'encore.js' 
-    ],
-    'stale':!!production,
-    'debug':!production
-  }
+﻿global.rootRequire = function (name) {
+  return require(__dirname + '/' + name);
 };
 
-app.configure(function (){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  // minify + combine
-  app.use(assetManager(assetManagerGroups)); 
-  // compress + cache
-  app.use(express.compress());
+global.__PORT = process.env.PORT || 3456;
+global.__PRODUCTION = !!process.env.PORT;
+global.__ROOT_PATH = __dirname;
+global.__ASSETS_VERSION = process.env.ASSET_FILES_VERSION || 51;
 
-  app.use(function (req, res, next) { 
-    if (/images\/|styles\/|scripts\//.test(req.url)) {
-      res.setHeader('Cache-Control', 'public, max-age=' + oneYear);
-      res.setHeader('Expires', new Date(Date.now() + oneYear).toUTCString());
+var 
+  express = require('express'),
+  engine = require('ejs-locals'),
+  compress = require('compression'),
+  //cookieParser = require('cookie-parser'),
+  //bodyParser = require('body-parser'),
+  mime = require('mime'),
+  app = express(),
+  oneYear = 31556908800,
+  assetManager = require('connect-assetmanager'),
+  assetManagerGroups = rootRequire('others/assetManagerGroups'),
+  request = require('request');
+
+app.engine('ejs', engine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+// compress + cache
+app.use(compress({
+  filter: function (req, res) {
+    if (/\.woff|\.ttf|\.svg|\.eot/.test(req.url)){
+      return true;
     }
-    return next();
-  });
-
-  app.use(express.static(__dirname + '/public', {maxAge: oneYear}));
-
-  // body parser
-  app.use(express.bodyParser());
-  // routes
-  app.use(app.router);
-  //mime types
-  mime.define({
-    'application/octet-stream': ['ttf'],
-    'image/svg+xml': ['svg'],
-    'application/vnd.ms-fontobject': ['eot'],
-    'application/x-font-woff': ['woff']
-  });
+    // fallback to standard filter function
+    return compress.filter(req, res)  
+  }  
+}));
+// minify + combine
+app.use(assetManager(assetManagerGroups)); 
+app.use(express.static(__dirname + '/public', {
+  maxAge: __PRODUCTION ? oneYear : -1
+}));
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended:true }));
+// app.use(bodyParser.json());
+// mime types
+mime.define({
+  'application/octet-stream': ['ttf'],
+  'image/svg+xml': ['svg'],
+  'application/vnd.ms-fontobject': ['eot'],
+  'application/x-font-woff': ['woff']
 });
 
 // routes
 app.get('/', function (req, res){
-  res.render('index', {production: production, ip: req.connection.remoteAddress});
+  res.render('index', {ip: req.connection.remoteAddress});
 });
 
-if (process.env.PORT && process.env.DYNO){
-  app.listen(process.env.PORT);
-} else {
-  // amazon and localhost
-  app.listen(3456);
-}
+app.get('/blog/:post?', function (req,res) {
+  var blogUrl = 'http://encore.wpengine.com/';
+  if (req.param.post) {
+    blogUrl += req.params.post;
+  }
+  request(blogUrl).pipe(res);
+});
+
+
+app.listen(__PORT);
---
+++ others/assetManagerGroups.js
@@ -0,0 +1,59 @@
+module.exports = (function (){
+
+  var oneYear = 31556908800;
+
+  function handleAssetCache(req, res, response, callback){
+
+    var headers = {
+      'Last-Modified': response.modified,
+      'Date': new Date().toUTCString(),
+      'Cache-Control': 'public, max-age=' + (__PRODUCTION ? oneYear : -1),
+      'Vary': 'Accept-Encoding'
+    }
+
+    if (req.headers['if-modified-since'] && Date.parse(req.headers['if-modified-since']) >= Date.parse(response.modified)) {
+      res.writeHead(304, headers);
+      res.end();
+      return;
+    }
+
+    headers['Content-Encoding'] = response.encoding;
+    headers['Content-Type'] = (function (url){
+      if (/\.css/.test(url)){
+        return 'text/css';
+      } else if (/\.js/.test(url)){
+        return 'application/javascript';
+      }
+    }(req.url));
+    headers['Content-Length'] = response.contentLength;
+
+    res.writeHead(200, headers);
+    res.end(response.contentBuffer);
+  }
+
+  return {
+    css: {
+      route: new RegExp('encore\.' + __ASSETS_VERSION + '\.min\.css'),
+      path: __ROOT_PATH + '/public/styles/',
+      dataType: 'css',
+      files: [ 
+        'fonts/proxima-nova/stylesheet.css', 
+        'fonts/roboto-slab/stylesheet.css',
+        'encore.css'
+      ],
+      serveModify: handleAssetCache
+    },
+    js: {
+      route: new RegExp('encore\.' + __ASSETS_VERSION + '\.min\.js'),
+      path: __ROOT_PATH + '/public/scripts/',
+      dataType: 'javascript',
+      files: [ 
+        'jquery-2.1.0.min.js', 
+        'funnel-animation.js',
+        'encore.js' 
+      ],
+      debug:!__PRODUCTION,
+      serveModify: handleAssetCache
+    }    
+  };
+}());
